5a4a53c913a6b79cbdc84dc6b8ff150c2f50ca21
mladez-temy <mladez.temy@gmail.com>
2025-04-26 18:44:40 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+    let actualindex = 1;
+    let max = 28;
+    function posun(kam) {	// primarne dostava cisla -1, +1
+        actualindex += kam;
+
+        if (actualindex < 1) 
+            actualindex = max; 
+        else if (actualindex > max)
+            actualindex = 1;
+        document.getElementById('velky').src = 'fotky/obr' + actualindex + '.jpg';
+    }
+    
+
+    function vymen(subor) {
+        document.getElementById("velky").src = subor;
+    }
