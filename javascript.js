2b036ac3583cc500a77c8f7a07e0feaeb115ff7c
Simon Skripec <simon.skripec@studen.ssnd.sk>
2025-06-01 18:17:00 +0200

    opravene_dvojmo

--- javascript.js
+++ javascript.js
@@ -15,18 +15,4 @@
         document.getElementById("velky").src = subor;
 
     let actualindex = 1;
-    let max = 28;
-    function posun(kam) {
-        actualindex += kam;
-
-        if (actualindex < 1) 
-            actualindex = max; 
-        else if (actualindex > max)
-            actualindex = 1;
-        document.getElementById('velky').src = 'fotky/obr' + actualindex + '.jpg';
-    }
-    
-
-    function vymen(subor) {
-        document.getElementById("velky").src = subor;
-    }}
+    let max = 28;}
