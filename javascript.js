f06238ddda9cb140dc27ebed62ac55ba23f51cbe
Simon Skripec <simon.skripec@studen.ssnd.sk>
2026-03-30 10:59:59 +0200

    filtrovanie tem, nika hoax

--- javascript.js
+++ javascript.js
@@ -11,6 +11,31 @@ function posun(kam) {	// primarne dostava cisla -1, +1
     }
     
 
-    function vymen(subor) {
+function vymen(subor) {
         document.getElementById("velky").src = subor;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const selectMenu = document.querySelector("#filter-clankov");
+    const clanky = document.querySelectorAll("article.celok");
+
+    function filtrujClanky() {
+        const vybranaHodnota = selectMenu.value;
+
+        clanky.forEach(clanok => {
+            // Ak je vybraté "vsetko" ALEBO sa ID zhoduje s výberom
+            if (vybranaHodnota === "vsetko" || clanok.id === vybranaHodnota) {
+                clanok.style.display = "block";
+            } else {
+                clanok.style.display = "none";
+            }
+        });
     }
+
+    selectMenu.addEventListener("change", filtrujClanky);
+
+    // Spustíme hneď, aby sa aplikoval počiatočný stav (napr. zobrazenie všetkého)
+    filtrujClanky();
+});
+
+console.log("JavaScript načítaný a pripravený na použitie.");
